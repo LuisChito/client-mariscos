593b81f297ca367009f5b6f69960b1cd05258c0b
LuisChito <lssalazarcanche1@gmail.com>
2026-03-15 04:09:11 -0600

    feat: agregar nuevo texto en el header

--- src/ui/dashboard/header/components/text.jsx
+++ src/ui/dashboard/header/components/text.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './heroText.css'
+import './text.css'
 
 const Text = () => {
   return (
     <div className="text">
       <h1>¿Se te antoja?</h1>
       <p>Conoce nuestro menú de exquisitos platillos</p>
-      <a href="/menu">Ver Nuestro Menú</a>
+      <a href="/menu">Ver Menú</a>
     </div>
   )
 }
--- src/ui/dashboard/header/pages/header.jsx
+++ src/ui/dashboard/header/pages/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/header';
 import Carrucel from '../components/carrucel';
-import Text from '../components/heroText';
+import Text from '../components/text';
 
 const Dashboard = () => {
   return (
