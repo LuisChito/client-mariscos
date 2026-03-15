db7ae2ccde7868a37e0e0052dafdb79a40837775
LuisChito <lssalazarcanche1@gmail.com>
2026-03-15 00:22:50 -0600

    feat: agregar componente Header con estilos e imagenes

--- src/ui/dashboard/components/header.jsx
+++ src/ui/dashboard/components/header.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import './header.css'
 
 const Header = () => {
-  return (
-    <div>
-      <h1>My Dashboard</h1>
+return (
+    <div className="header">
+        <div className="header-logo">
+            <img src="/imgs/favicon.png" alt="Logo" className="logo" />
+        </div>
+        <div>
+            <nav className="header-nav">
+                <a href="/contact">Contactanos</a>
+                <a href="#menu">Ver Menú</a>
+                <a href="#reservations">Reservas</a>
+            </nav>
+        </div>
     </div>
-  )
+)
 }
 
 export default Header
