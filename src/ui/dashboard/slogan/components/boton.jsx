34064233c136f0aa5c8a25a10e720b93b0dc782c
LuisChito <lssalazarcanche1@gmail.com>
2026-03-17 14:18:45 -0600

    feat: agregar boton para del componente Slogan

---
+++ src/ui/dashboard/slogan/components/boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './boton.css'
+
+const boton = () => {
+  return (
+    <div className="boton">
+    <a href="/restaurantes">Sucursales</a>
+    </div>
+  )
+}
+
+export default boton
--- src/ui/dashboard/slogan/pages/slogan.jsx
+++ src/ui/dashboard/slogan/pages/slogan.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import SloganContent from '../components/slogan'
+import Boton from '../components/boton'
 
 const Slogan = () => {
   return (
+    <>
     <SloganContent />
+    <Boton />
+    </>
   )
 }
 
