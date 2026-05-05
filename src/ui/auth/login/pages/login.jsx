20fe92696938e00b398c9cb1ce71d0df8866e7cd
LuisChito <lssalazarcanche1@gmail.com>
2026-05-05 00:12:13 -0600

    fix: ajustar

--- src/ui/auth/login/pages/login.jsx
+++ src/ui/auth/login/pages/login.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import LoginCard from '../components/loginCard'
 const Login = () => {
   return (
-    <div>
-        <LoginCard/>
-    </div>
+    <>
+    <LoginCard/>
+    </>
   )
 }
 
