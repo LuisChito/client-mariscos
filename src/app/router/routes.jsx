7eab47dbef831bca743004b65e515eb27decfba2
LuisChito <lssalazarcanche1@gmail.com>
2026-05-10 00:17:45 -0600

    feat: agregar proteccion a las rutas de admin

--- src/app/router/routes.jsx
+++ src/app/router/routes.jsx
@@ -1,12 +1,27 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 
 import Dashboard from '@ui/dashboard/pages/dashboard'
 import Sucursales from '@ui/sucursales/pages/sucursales'
 
+// admin
+import FormularioQuejas from '@ui/dashboard/formulario/pages/formulario'
+
 // Login
 import Login from '@ui/auth/login/pages/login'
 
+
+const rutasProtegidas = ({children}) => {
+	const sesion = localStorage.getItem('user_session');
+	// si no hay una sesion lo redirigimos al login
+	if (!sesion) {
+		return <Navigate to="/login" replace />;
+	}
+	// si hay sesion mostramos el elemento children
+    return children;
+}
+
 export const router = createBrowserRouter([
+	// Rutas Normales
 	{
 		path: '/',
 		element: <Dashboard />,
@@ -14,6 +29,16 @@ export const router = createBrowserRouter([
 	{
 		path: '/sucursales',
 		element: <Sucursales />,
+	}, 
+
+	// Rutas admin
+	{
+		path: 'admin/sucursales',
+		element: (
+			<rutasProtegidas>
+				<FormularioQuejas/>
+			</rutasProtegidas>
+		)
 	},
 
 	//Login
@@ -21,4 +46,5 @@ export const router = createBrowserRouter([
 		path: '/login',
 		element: <Login />,
 	}
+
 ])
