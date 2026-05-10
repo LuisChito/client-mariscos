9e9347241d48cbd8584fb69197ef03d6d2c7388a
LuisChito <lssalazarcanche1@gmail.com>
2026-05-10 13:37:40 -0600

    feat: añadir componente Sucursales e integrarlo en la página sucursalesAdmin

---
+++ src/ui/admin/sucursales/components/sucursales.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const sucursales = () => {
+  return (
+    <div>sucursales</div>
+  )
+}
+
+export default sucursales
---
+++ src/ui/admin/sucursales/pages/sucursalesAdmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Sucursales from '../components/sucursales'
+
+const sucursalesAdmin = () => {
+  return (
+    <Sucursales/>
+  )
+}
+
+export default sucursalesAdmin
