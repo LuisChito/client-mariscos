157b39fde7a34e2c9313dad558e435ad0974501a
LuisChito <lssalazarcanche1@gmail.com>
2026-05-10 00:38:02 -0600

    feat: implementar lógica de inicio de sesión y manejo de estado en LoginCard

--- src/ui/auth/login/components/loginCard.jsx
+++ src/ui/auth/login/components/loginCard.jsx
@@ -1,21 +1,63 @@
 import React from 'react'
 import './loginCard.css'
 import favicon from '../../../../../public/imgs/favicon.png'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginCard = () => {
+  // Creamos estados para guardar a los usuarios
+  const [usuario, setUsuario] =  useState('');
+  const [password, setPassword] = useState('');
+  // Redirrecionmiento cuando sí sale bien el login 
+  const navigate = useNavigate();
 
-let iniciarSesion = () => {
-  
-}
+  const iniciarSesion = async () => {
+    try {
+      const respuesta = await fetch('http://localhost:3001/loginAuth', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify({ usuario, password }), // Enviamos los datos 
+      });
+      const datos = await respuesta.json();
+      if (respuesta.ok) {
+        const datosGuardar = {
+        id: datos.id,
+        usuario: datos.usuario
+      };
+        // Aqui guardamos el perfil si el inicio de sesion es exitoso
+        localStorage.setItem('user_session', JSON.stringify(datosGuardar));
+        console.log('Inicio de sesion exitoso');
+        navigate('/admin/sucursales');
+      }
+      else{
+        console.log('Inicio de sesion fallido');
+        navigate('/logi');
+      }
+    } catch (error) {
+      console.error('Error de conexion: ', error);
+      alert('Error del servidor');
+    }
+  };
+
   return (
     <div className='contenedor-login'>
       <p className='txt'>Inicia sesión</p>
       <form>
         <label htmlFor="username">Usuario: </label>
-        <input type="text" id="usuario" name="usuario" />
+        <input 
+          type="text" 
+          id="usuario" 
+          value={usuario}
+          onChange={(e) => setUsuario(e.target.value)} // Actualiza el estado de usuario
+        />
         <br />
         <label htmlFor="password">Contraseña: </label>
-        <input type="password" id="password" name="password" />
+        <input 
+          type="password" 
+          id="password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} // Actualiza el estado de la contraseñá
+        />
       </form>
       <p className="info">Dale click a la imagen para iniciar sesión</p>
       <img src={favicon} alt="Logo" className='logo' onClick={iniciarSesion} />
