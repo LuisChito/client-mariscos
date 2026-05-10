700ec76646c1459994a3e48be2c2ac8a3538ff9e
LuisChito <lssalazarcanche1@gmail.com>
2026-05-10 14:46:51 -0600

    feat: añadir componente Header a la página sucursalesAdmin

--- src/ui/admin/sucursales/pages/sucursalesAdmin.jsx
+++ src/ui/admin/sucursales/pages/sucursalesAdmin.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Sucursales from '../components/sucursales'
+import Header from '../../components/header'
 
 const sucursalesAdmin = () => {
   return (
+    <>
+    <Header/>
     <Sucursales/>
+    </>
   )
 }
 
