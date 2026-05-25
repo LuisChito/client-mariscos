a9b0e00f7d9742a19188ddcc6bba7a950d26a844
LuisChito <lssalazarcanche1@gmail.com>
2026-05-24 22:24:21 -0600

    feat: refactorizar componente Video y actualizar estilos para mejorar la legibilidad

--- src/ui/dashboard/video/Video.jsx
+++ src/ui/dashboard/video/Video.jsx
@@ -4,10 +4,10 @@ import './video.css'
 const Video = () => {
   return (
     <>
-    <div className="cont">
-        <div className="card">
-            <div className="text">
-                <p className='textoloco'>Checa nuestro video</p>
+    <div className="video-container">
+      <div className="video-card">
+        <div className="video-text">
+          <p className='video-title'>Checa nuestro video</p>
             </div>
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/df__5RAn6Ok?si=IGhBLWrhpZFvHUAr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
