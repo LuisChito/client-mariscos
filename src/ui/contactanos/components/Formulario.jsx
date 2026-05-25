4644f9c3eaaff379c64a9d52811092fa6da056fb
LuisChito <lssalazarcanche1@gmail.com>
2026-05-24 23:23:51 -0600

    feat: implementar funcionalidad createContacto y añadir rutas de contacto

--- src/ui/contactanos/components/Formulario.jsx
+++ src/ui/contactanos/components/Formulario.jsx
@@ -2,31 +2,50 @@ import React from 'react'
 import './Formulario.css'
 
 const Formulario = () => {
+    const enviarFormulario = async (e) => {
+        e.preventDefault()
+        const data = new FormData(e.currentTarget)
+
+        const respuesta = await fetch('http://localhost:3001/contacto', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                nombre: data.get('nombre'),
+                motivo: data.get('motivo'),
+                mensaje: data.get('mensaje')
+            })
+        })
+        if (respuesta.ok) {
+            alert('Mensaje enviado')
+            e.currentTarget.reset()
+            } else {
+            alert('Error al enviar')
+            }
+        }
+
   return (
     <div className="contenedorFormulario">
-        <div className="textoPq">
-            <p>Contactanos</p>
-        </div>
-        <form action="">
-            <div className="">
-                <label htmlFor="nombre">Nombre completo:</label>
-                <input type="text" id="nombre" required />
-                <br />
-                <label htmlFor="motivo">Motivo de contacto: </label>
-                <select name="" id="motivo" required>
-                    <option value="">Selecciona</option>
-                    <option value="Comida">Comida</option>
-                    <option value="Servicio">Servicio</option>
-                    <option value="Ventas">Ventas</option>
-                </select>
-                <br />
-                <label htmlFor="mensaje">Redacta tu mensaje</label>
-                <br />
-                <textarea name="name" id="mensaje" required ></textarea>
-                <br />
-                <button type='submit'>Enviar</button>
-            </div>
-        </form>
+      <div className="textoPq">
+        <p>Contactanos</p>
+      </div>
+
+      <form onSubmit={enviarFormulario}>
+        <label htmlFor="nombre">Nombre completo:</label>
+        <input type="text" id="nombre" name="nombre" required />
+
+        <label htmlFor="motivo">Motivo de contacto:</label>
+        <select id="motivo" name="motivo" required>
+          <option value="">Selecciona</option>
+          <option value="Comida">Comida</option>
+          <option value="Servicio">Servicio</option>
+          <option value="Ventas">Ventas</option>
+        </select>
+
+        <label htmlFor="mensaje">Redacta tu mensaje</label>
+        <textarea id="mensaje" name="mensaje" required />
+
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   )
 }
