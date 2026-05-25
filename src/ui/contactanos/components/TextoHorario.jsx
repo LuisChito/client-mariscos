c26799c681a18f8f1c9de317ce52b64c26eb5b01
LuisChito <lssalazarcanche1@gmail.com>
2026-05-25 09:13:00 -0600

    feat: añadir componente TextoHorario y estilos asociados en la página de contacto

---
+++ src/ui/contactanos/components/TextoHorario.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TextoHorario.css'
+
+const TextoHorario = () => {
+  return (
+    <div className='cont-thorario'>
+        <p>Nuestro horario de atención</p>
+    </div>
+  )
+}
+
+export default TextoHorario
--- src/ui/contactanos/pages/Contactanos.jsx
+++ src/ui/contactanos/pages/Contactanos.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../components/header'
 import Formulario from '../components/Formulario'
 import Horario from '../components/Horario'
+import TextoHorario from '../components/TextoHorario'
 
 const Contactanos = () => {
   return (
     <>
     <Header/>
     <Formulario/>
+    <TextoHorario/>
     <Horario/>
     </>
   )
